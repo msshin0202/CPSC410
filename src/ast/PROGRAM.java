49a0d3ef9088ef32675418b7e1a7afe39790f4fe
hurjun1995 <hurjun1995@gmail.com>
2019-10-12 11:45:44 -0700

    added skeletal files

---
+++ src/ast/PROGRAM.java
@@ -0,0 +1,15 @@
+package ast;
+
+import libs.Node;
+
+public class PROGRAM extends Node {
+    @Override
+    public void parse() {
+
+    }
+
+    @Override
+    public String evaluate() {
+        return null;
+    }
+}
